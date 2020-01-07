8b014b73aac9b7f4855dd146f7c8427023f8a803
Ashish Yadav <iloveh4ck1ng@gmail.com>
2020-01-07 14:24:06 +0545

    update

--- page.php
+++ page.php
@@ -9,7 +9,6 @@
 	<div id="main-content">
 
 		<div class="container">
-
 			<div class="col-md-8">
 
 				<?php 
@@ -29,7 +28,26 @@
 				endif;
 
 				?>
+				<?php 
+					if(isset($_POST['submit_users'])) {
+						$user = $_POST['get_users'];
+						$query = "SELECT * FROM wp_users WHERE user_login = $user ";
+						echo $query;
+						$results = $wpdb->get_results($query);
+						var_dump($results);
+					}
+				?>
+				<form method="POST">
+					<div class="get-users">
+						<input type="text" name="get_users">
+						<input type="submit" name="submit_users" value="Get Users">
+					</div>
+				</form>
 
+			<!-- <div style="border: 2px solid #D5CC5A; overflow: hidden; margin: 15px auto; max-width: 575px;">
+				<iframe scrolling="no" src="http://whitehatagency.com.au/" style="border: 0px none; margin-left: -36px; height: 812px; margin-top: -486px; width: 650px;">
+				</iframe>
+			</div> -->
 			</div>
 
 			<div class="col-md-4">
